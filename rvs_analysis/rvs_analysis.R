6d342e581ab3034c084cacc907b226a282269192
0xneptune <0xneptune@users.noreply.github.com>
2017-11-18 16:37:29 -0500

    Work in progress R script

--- rvs_analysis/rvs_analysis.R
+++ rvs_analysis/rvs_analysis.R
@@ -5,10 +5,29 @@
 #
 ###############################################################################
 
+# TODO: These should be read in from the CVS.
+largeGainValue = 10
+smallGainValue = 2
+largeLossValue = -9
+smallLossValue = -1 
+
 workingDirectoryPath = getwd()
 csvDirectoryPath = paste(workingDirectoryPath, "data", sep = "/")
 rawDataInquisitPath = paste(csvDirectoryPath, "rvs_v4_rawdata_inquisit.csv",
                             sep = "/")
 
-rawDataInquisit=read.csv(rawDataInquisitPath)
-print(rawDataInquisit)
+rawDataInquisit = read.csv(rawDataInquisitPath)
+subjectIds = rawDataInquisit["subject"]
+blockCodeValues = rawDataInquisit["blockcode"]
+
+for (index in 1:lengths(blockCodeValues)) {
+  if (!identical(blockCodeValues[index], "attentionQuiz")) {
+    print("Skipping")
+    next
+  }
+  
+  print(blockCodeValues[index])
+  subjectId = subjectIds[index]
+}
+
+
