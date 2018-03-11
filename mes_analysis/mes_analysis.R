86e183bad2a0975b40c4a0dd12d2ec85afa93ff9
0xneptune <0xneptune@users.noreply.github.com>
2018-03-10 20:26:18 -0500

    [MES] Initial script setup - CSV file reads

---
+++ mes_analysis/mes_analysis.R
@@ -0,0 +1,48 @@
+###############################################################################
+# MES (Moral Evaluation Study) Analysis
+#
+# AUTHORS: Neda Safaee-Rad, Computational Affective Neuroscience (CAN) Lab,
+#          U of T
+#
+###############################################################################
+
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+
+
+# Placeholder
+
+
+###############################################################################
+# PROGRAM
+###############################################################################
+
+
+# Load and read the CSV
+workingDirectoryPath = getwd()
+
+csvDirectoryPath = paste(workingDirectoryPath, "data", sep = "/")
+
+qualtricsDataPath = paste(
+  csvDirectoryPath,
+  "qualtrics_IN_LAB_Moral_Evaluation_January_8_2018_16_01_numeric_cleanedcolnames.csv",
+  sep = "/"
+)
+
+rawDataPath = paste(
+  csvDirectoryPath,
+  "raw_evaluative_categorization_mar13_2017_p_m_18_01_09.csv",
+  sep = "/"
+)
+
+summaryDataPath = paste(
+  csvDirectoryPath,
+  "summary_evaluative_categorization_mar13_2017_p_m_18_01_08-2.csv",
+  sep = "/"
+)
+
+qualtricsData = read.csv(qualtricsDataPath, stringsAsFactors = FALSE)
+rawData       = read.csv(rawDataPath, stringsAsFactors = FALSE)
+summaryData   = read.csv(summaryDataPath, stringsAsFactors = FALSE)
